bb1de734f081272f3b02241ad88f3013cd0988ca
Mona Lisa <notoriouskro@gmail.com>
2019-01-04 20:11:27 -0800

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i" , "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+for (var i = Math.floor(Math.random()*alpha.length);
+
+console.log(i)
+
+// for (var i = a; i < alpha.length; i++)
+    // if (alpha[i])
